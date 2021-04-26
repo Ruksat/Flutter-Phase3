f513cecf9e6a01d2967c965d16544eaf3bd0f0a0
Ruksat <ruksat.sol@student.mahidol.ac.th>
2021-04-26 23:02:56 +0700

    Add Files

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# flutter_app
+# Following a user with Google Maps for Flutter
 
-A new Flutter application.
+A sample Flutter application that demonstrates the use of Geolocator to stream the device position and adjust the camera position of a Google map to follow the user.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+A Google Map key is required.  Once obtained replace the "YOUR_KEY" in AndroidManifest.xml and AppDelegate.swift with your key.  You will also need to enable the Maps SDK for Android and Maps SDK for IOS on your google project console.  
 
-A few resources to get you started if this is your first Flutter project:
+## Video
+https://www.youtube.com/watch?v=Bn9e6LZQU9I
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Article
+https://www.julow.com/article/2
--- lib/main.dart
+++ lib/main.dart
@@ -1,236 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:mapfollow/services/geolocator_service.dart';
+import 'package:provider/provider.dart';
+import './screens/map.dart';
 
- void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
- class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
- return MaterialApp(
- debugShowCheckedModeBanner: false,
- title: 'Book Store',
- theme: ThemeData(
- primarySwatch: Colors.teal,
- ),
- home: Scaffold(
- appBar: AppBar(title: Text('Book Store')),
- body: BodyLayout(),
- ),
- );
- }
- }
- //item price
-int PriceChemistryBook = 299;
-int PriceMathematicsBook = 300;
-int PricePhysicalBook = 450;
-int PriceEnglishBook = 320;
-int PriceGeographyBook = 500;
-int PriceAccountingBook = 550;
-int PriceLifeSciencesBook = 490;
-int PriceHistoryBook = 650;
-int PriceHealtandPhysicalEducationBook = 370;
-int PriceEconomicsBook = 440;
-int PriceTheoryofMusicBook = 590;
-int TotalPrice = 0;
-int ItemCount = 0;
-
- class BodyLayout extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
- return _myListView(context);
- }
- }
-
-Widget _myListView(BuildContext context) {
-  return ListView(
-  children: <Widget>[
-  ListTile(
-      leading: CircleAvatar(
-        backgroundImage: AssetImage('assets/images/Chemistry.jpg'),
+class MyApp extends StatelessWidget {
+  final geoService = GeolocatorService();
+  @override
+  Widget build(BuildContext context) {
+    return FutureProvider(
+      create: (context) => geoService.getInitialLocation(),
+      child: MaterialApp(
+        title: 'Tracking',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Consumer<Position>(
+          builder: (context, position, widget) {
+            return (position != null)
+                ? Map(position)
+                : Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
-      title: Text('Chemistry For Grade 11'),
-      subtitle: Text('Price: \฿299'),
-      trailing: Icon(Icons.add),
-      onTap: () {
-        TotalPrice += PriceChemistryBook;
-        ItemCount++;
-        print('total item: $ItemCount');
-        print('total price: $TotalPrice');
-        Scaffold.of(context).showSnackBar(SnackBar(
-          content: Text(
-            'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-      },
-    ),
-  ListTile(
-      leading: CircleAvatar(
-        backgroundImage: AssetImage('assets/images/Mathematics.jpg'),
-      ),
-      title: Text('Mathematics For Grade 11'),
-      subtitle: Text('Price: \฿300'),
-      trailing: Icon(Icons.add),
-      onTap: () {
-        TotalPrice += PriceMathematicsBook;
-        ItemCount++;
-        print('total item: $ItemCount');
-        print('total price: $TotalPrice');
-        Scaffold.of(context).showSnackBar(SnackBar(
-            content: Text(
-                'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-      },
-    ),
-  ListTile(
-      leading: CircleAvatar(
-        backgroundImage: AssetImage('assets/images/Physical.png'),
-      ),
-      title: Text('Physical For Grade 11'),
-      subtitle: Text('Price: \฿450'),
-      trailing: Icon(Icons.add),
-      onTap: () {
-        TotalPrice += PricePhysicalBook;
-        ItemCount++;
-        print('total item: $ItemCount');
-        print('total price: $TotalPrice');
-        Scaffold.of(context).showSnackBar(SnackBar(
-            content: Text(
-                'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-      },
-    ),
-  ListTile(
-      leading: CircleAvatar(
-        backgroundImage: AssetImage('assets/images/English.jpg'),
-      ),
-      title: Text('English For Grade 11'),
-      subtitle: Text('Price: \฿320'),
-      trailing: Icon(Icons.add),
-      onTap: () {
-        TotalPrice += PriceEnglishBook;
-        ItemCount++;
-        print('total item: $ItemCount');
-        print('total price: $TotalPrice');
-        Scaffold.of(context).showSnackBar(SnackBar(
-            content: Text(
-                'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-      },
-    ),
-  ListTile(
-      leading: CircleAvatar(
-        backgroundImage: AssetImage('assets/images/Geography.jpg'),
-      ),
-      title: Text('Geography For Grade 11'),
-      subtitle: Text('Price: \฿500'),
-      trailing: Icon(Icons.add),
-      onTap: () {
-        TotalPrice += PriceGeographyBook;
-        ItemCount++;
-        print('total item: $ItemCount');
-        print('total price: $TotalPrice');
-        Scaffold.of(context).showSnackBar(SnackBar(
-            content: Text(
-                'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-      },
-    ),
-  ListTile(
-      leading: CircleAvatar(
-        backgroundImage: AssetImage('assets/images/Accounting.jpg'),
-      ),
-      title: Text('Accounting For Grade 11'),
-      subtitle: Text('Price: \฿550'),
-      trailing: Icon(Icons.add),
-      onTap: () {
-        TotalPrice += PriceAccountingBook;
-        ItemCount++;
-        print('total item: $ItemCount');
-        print('total price: $TotalPrice');
-        Scaffold.of(context).showSnackBar(SnackBar(
-            content: Text(
-                'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-      },
-    ),
-  ListTile(
-  leading: CircleAvatar(
-  backgroundImage: AssetImage('assets/images/LifeSciences.jpg'),
-  ),
-  title: Text('Life Sciences For Grade 11'),
-  subtitle: Text('Price: \฿490'),
-  trailing: Icon(Icons.add),
-  onTap: () {
-    TotalPrice += PriceLifeSciencesBook;
-    ItemCount++;
-    print('total item: $ItemCount');
-    print('total price: $TotalPrice');
-    Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text(
-            'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-  },
-  ),
-  ListTile(
-  leading: CircleAvatar(
-  backgroundImage: AssetImage('assets/images/History.jpg'),
-  ),
-  title: Text('History For Grade 11'),
-  subtitle: Text('Price: \฿650'),
-  trailing: Icon(Icons.add),
-  onTap: () {
-    TotalPrice += PriceHistoryBook;
-    ItemCount++;
-    print('total item: $ItemCount');
-    print('total price: $TotalPrice');
-    Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text(
-            'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-  },
-  ),
-  ListTile(
-  leading: CircleAvatar(
- backgroundImage: AssetImage('assets/images/HealtandPhysicalEducation.jpg'),
-  ),
-  title: Text('Health & Physical Education For Grade 11'),
-  subtitle: Text('Price: \฿370'),
-  trailing: Icon(Icons.add),
-  onTap: () {
-  TotalPrice += PriceHealtandPhysicalEducationBook;
-  ItemCount++;
-  print('total item: $ItemCount');
-  print('total price: $TotalPrice');
-  Scaffold.of(context).showSnackBar(SnackBar(
-      content: Text(
-          'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-  },
-  ),
-  ListTile(
-  leading: CircleAvatar(
-  backgroundImage: AssetImage('assets/images/Economics.jpg'),
-  ),
-  title: Text('Economics For Grade 11'),
-  subtitle: Text('Price: \฿440'),
-  trailing: Icon(Icons.add),
-  onTap: () {
-    TotalPrice += PriceEconomicsBook;
-    ItemCount++;
-    print('total item: $ItemCount');
-    print('total price: $TotalPrice');
-    Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text(
-            'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-  },
-  ),
-  ListTile(
-  leading: CircleAvatar(
-  backgroundImage: AssetImage('assets/images/TheoryofMusic.jpg'),
-  ),
-  title: Text('Theory of music For Grade 4'),
-  subtitle: Text('Price: \฿590'),
-  trailing: Icon(Icons.add),
-  onTap: () {
-    TotalPrice += PriceTheoryofMusicBook;
-    ItemCount++;
-    print('total item: $ItemCount');
-    print('total price: $TotalPrice');
-    Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text(
-            'Add 1 more item\nTotal item: $ItemCount\nTotal Price: $TotalPrice')));
-  },
-  ),
-  ],
-  );
+    );
   }
-
+}
